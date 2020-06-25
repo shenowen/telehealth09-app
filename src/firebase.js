99e9f2f68705967fdc56a10e51a72298a8b8a4a9
shenowen36 <shenowen36@gmail.com>
2020-06-25 15:45:04 -0500

    Initial commit with firebase app, bootstrap included

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBSwh2D0CVgRH2xnI7_2Q1RsPTjWah1wjE",
+    authDomain: "telehealth09-app.firebaseapp.com",
+    databaseURL: "https://telehealth09-app.firebaseio.com",
+    projectId: "telehealth09-app",
+    storageBucket: "telehealth09-app.appspot.com",
+    messagingSenderId: "452490710426",
+    appId: "1:452490710426:web:1dbe2d7c232641cbe6fe23"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
