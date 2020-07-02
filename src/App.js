ec3378cacba3e52da34e7a5b8f8bd0c91434eaa2
Karim Soufan <ks562@evansville.edu>
2020-07-01 20:51:26 -0400

    Added SignUp and SingUp functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import firebase from './firebase';
 
 function App() {
   return (
@@ -7,6 +8,20 @@ function App() {
       Hello World Telehealth09-app
     </div>
   );
+}
+
+async function SignIn() {
+  const email = 'ks562@evansville.edu';
+  const pass = 'test';
+  const data = await firebase.auth().SignInWithEmailAndPassword(email, pass);
+
+
+}
+
+async function SignUp() {
+  const email = 'ks562@evansville.edu';
+  const pass = 'test';
+  const data = await firebase.auth().createUserWithEmailAndPassword(email, pass);
 }
 
 export default App;
